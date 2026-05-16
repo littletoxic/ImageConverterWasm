57f0c9bd12bd384e651417bd2baee089ab1c495e
littletoxic <1325218784@qq.com>
2026-05-16 17:38:55 +1000

    Organize model files by responsibility

--- src/ImageConverter/Models/UnionCompileCheck.cs
+++
@@ -1,9 +0,0 @@
-namespace ImageConverter.Models;
-
-internal sealed record UnionCompileCheckSuccess;
-
-internal sealed record UnionCompileCheckFailure(string Message);
-
-internal union UnionCompileCheckResult(
-    UnionCompileCheckSuccess,
-    UnionCompileCheckFailure);
